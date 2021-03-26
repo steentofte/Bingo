ce96ed835200cc12cd553479395f5bc208bf381f
steentofte <steentofte22@hotmail.com>
2021-03-27 00:48:11 +0100

    83

--- Randomgenerator.js
+++ Randomgenerator.js
@@ -25,10 +25,14 @@ function PrintCurrentTal (NytTal){
     document.write(NytTal);
 }
 
-function addRow () 
-    {
-      <div class="row">
-        <input type="text" name="name" value = {NytTal} />
-      </div>      
-    }
+<script>
+function myFunction() {
+  var table = document.getElementById("myTable");
+  var row = table.insertRow(0);
+  var cell1 = row.insertCell(0);
+  var cell2 = row.insertCell(1);
+  cell1.innerHTML = "NEW CELL1";
+  cell2.innerHTML = "NEW CELL2";
+}
+</script>
   
