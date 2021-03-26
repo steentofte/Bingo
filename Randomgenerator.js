81aab39912a1a3f57849372d6bd2594602c1d064
steentofte <steentofte22@hotmail.com>
2021-03-27 00:35:38 +0100

    81

--- Randomgenerator.js
+++ Randomgenerator.js
@@ -24,3 +24,14 @@ function NytTalPull (){
 function PrintCurrentTal (NytTal){
     document.write(NytTal);
 }
+
+function printDiv(divName) {
+    var printContents = document.getElementById(divName).innerHTML;
+    var originalContents = document.body.innerHTML;
+
+    document.body.innerHTML = printContents;
+
+    window.print();
+
+    document.body.innerHTML = originalContents;
+}
