b42b31e46b5f74d3bc3de1a546c9be0bc712014d
steentofte <steentofte22@hotmail.com>
2021-03-26 23:46:09 +0100

    68

--- Randomgenerator.js
+++ Randomgenerator.js
@@ -2,10 +2,10 @@ function randomNumber(minimum, maximum){
     return Math.round( Math.random() * (maximum - minimum) + minimum);
 }
 
-function NytTalpull (){
+function NytTalPull (){
     var NytTal = randomNumber( 1, 90 );
     UdtrukneTal.push (NytTal);
-    PrintCurrentTal(NytTal)
+    PrintCurrentTal(NytTal);
 }
 
 function PrintCurrentTal (NytTal){
