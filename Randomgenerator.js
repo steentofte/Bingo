9f1bf2d62d7a6210e8570674d89b8a8e48de9ba6
steentofte <steentofte22@hotmail.com>
2021-03-27 00:53:08 +0100

    86

--- Randomgenerator.js
+++ Randomgenerator.js
@@ -1,6 +1,7 @@
 var rollDie = randomNumber( 1, 5 );
 var CurrentTal = rollDie;
 var UdtrukneTal=[];
+var table = document.getElementById("PrintTal");
 
 function randomNumber(minimum, maximum){
     return Math.round( Math.random() * (maximum - minimum) + minimum);
@@ -26,10 +27,7 @@ function PrintCurrentTal (NytTal){
 }
 
 function myFunction() {
-  var table = document.getElementById("myTable");
   var row = table.insertRow(0);
   var cell1 = row.insertCell(0);
-  var cell2 = row.insertCell(1);
-  cell1.innerHTML = "NEW CELL1";
-  cell2.innerHTML = "NEW CELL2";
+  cell1.innerHTML = NytTal;
 }  
