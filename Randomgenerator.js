f20c270cc5f9d2fad7fdcbbbbb74525972991c66
steentofte <steentofte22@hotmail.com>
2021-03-27 01:11:12 +0100

    94

--- Randomgenerator.js
+++ Randomgenerator.js
@@ -18,6 +18,10 @@ function NytTalPull (){
             //PrintCurrentTal(NytTal);
             UdtrukneTal.push (NytTal);
             console.log (UdtrukneTal);
+            var table = document.getElementById("MyTable");
+            var row = table.insertRow(0);
+            var cell1 = row.insertCell(0);
+            cell1.innerHTML = NytTal;
         }
 }
 
