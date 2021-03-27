6f4c8c3102c62daea903b03439e12f1ec91679d3
steentofte <steentofte22@hotmail.com>
2021-03-27 14:18:04 +0100

    Update BingoPladeMatrix.js

--- BingoPladeMatrix.js
+++ BingoPladeMatrix.js
@@ -17,5 +17,25 @@ function NewPlate(){
         console.log(PladeNummer);
         BingoArray.push (PladeNummer);
         document.getElementById(i).innerHTML = PladeNummer;
+        BingoMatrix();
     }
+}
+
+function BingoMatrix(){
+    var TotalN = 0;
+    while (TotalN !== 15){
+        var R1 = randomNumber( 1, 3 );
+        var R2 = randomNumber( 1, 3 );
+        var R3 = randomNumber( 1, 3 );
+        var R4 = randomNumber( 1, 3 );
+        var R5 = randomNumber( 1, 3 );
+        var R6 = randomNumber( 1, 3 );
+        var R7 = randomNumber( 1, 3 );
+        var R8 = randomNumber( 1, 3 );
+        var R9 = randomNumber( 1, 3 );
+    TotalN = R1+R2+R3+R4+R5+R6+R7+R8+R9;
+    console.log (TotalN);
+}
+
+
 }
