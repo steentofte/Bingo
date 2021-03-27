0ed3e9a0195705f0524502d9bbdae8c8f229c033
steentofte <steentofte22@hotmail.com>
2021-03-27 01:03:27 +0100

    88

--- Randomgenerator.js
+++ Randomgenerator.js
@@ -26,8 +26,10 @@ function PrintCurrentTal (NytTal){
 }
 
 function myFunction() {
-    var table = document.getElementById("PrintTal");
+    var table = document.getElementById("myTable");
     var row = table.insertRow(0);
     var cell1 = row.insertCell(0);
-    cell1.innerHTML = "NytTal";
-}  
+    var cell2 = row.insertCell(1);
+    cell1.innerHTML = "NEW CELL1";
+    cell2.innerHTML = "NEW CELL2";
+  }
