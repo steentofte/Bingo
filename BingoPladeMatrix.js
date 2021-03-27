16de94d180a1ad714055858599742a47be0bdf32
steentofte <steentofte22@hotmail.com>
2021-03-27 13:46:32 +0100

    110

--- BingoPladeMatrix.js
+++ BingoPladeMatrix.js
@@ -1,4 +1,4 @@
-<script src="Randomgenerator.js"></script>
+var rollDie = randomNumber( 1, 90 );
 var MaxNumber = 15;
 var MaxLinje = 3;
 var MaxSpread = 10;
@@ -6,6 +6,9 @@ var StartTal = 1;
 var SlutTal = 90;
 var BingoArray = [];
 
+function randomNumber(minimum, maximum){
+    return Math.round( Math.random() * (maximum - minimum) + minimum);
+}
 
 function BingoPladeGenerator (){
     for (i = 0; i < MaxNumber; i++) {
