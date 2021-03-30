f49d61870e777eddb62945e6d95513a91f187e2f
steentofte <steentofte22@hotmail.com>
2021-03-31 00:25:04 +0200

    Update BingoPladeMatrix.js

--- BingoPladeMatrix.js
+++ BingoPladeMatrix.js
@@ -107,7 +107,7 @@ function MakeRows (){
             const randB = Math.random() < 0.5;
             const randC = Math.random() < 0.5;
             var kolonne = [randA];
-            console.log(kolonne.includes(Boolean));
+            console.log(kolonne);
             //while (PladeNy.includes(NewNumber)){
                 
 
