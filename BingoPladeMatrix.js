9de07bb97e16b91cf5cbdbbf4f681c23f27e1dbd
steentofte <steentofte22@hotmail.com>
2021-03-30 23:54:45 +0200

    Update BingoPladeMatrix.js

--- BingoPladeMatrix.js
+++ BingoPladeMatrix.js
@@ -84,8 +84,10 @@ function GenererPlade(){
     var NytTal80 = randomNumber( 71, 80 );
     var NytTal90 = randomNumber( 81, 90 );
 
-for (x = 0; x < RTotal[x]; x++){
+for (x = 0; x < 9; x++){
+    for (y=0; y < RTotal[x]; y++){
     console.log(x,"*");
+    }
 }
     /*tal1 = NytTal10;
     console.log(tal1);
