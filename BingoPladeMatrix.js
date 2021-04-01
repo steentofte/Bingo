0f4e71ddb73d773c889a8e110483e8df0b8c7786
steentofte <steentofte22@hotmail.com>
2021-04-01 16:41:55 +0200

    Update BingoPladeMatrix.js

--- BingoPladeMatrix.js
+++ BingoPladeMatrix.js
@@ -151,7 +151,11 @@ function PlateLayout (){
     Placering.push(kolonne);
     
 
-console.log(Placering);
+console.table(Placering);
+//Placering.push([0,0],[1,0],[2,0],[3,0],[4,0],[5,0],[6,0],[7,0],[8,0],[9,0]);
+
+
+
 }
 
 function Fake() {
