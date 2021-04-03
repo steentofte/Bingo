75902263ddc0a1ac68dfcd990fbebc2d23c0ef19
steentofte <steentofte22@hotmail.com>
2021-04-03 13:11:59 +0200

    Update BingoPladeMatrix.js

--- BingoPladeMatrix.js
+++ BingoPladeMatrix.js
@@ -195,8 +195,7 @@ function VisPlade() {
         {
         for (col = 0; col < MaxLinje; col++)
         console.log(layout[col][r]);
-       
-        y =layout[col][r];
+        y = layout[col][r];
         console.log(y);
 
 
